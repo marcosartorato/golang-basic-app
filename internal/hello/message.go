ded1a5fc9e302fff78bf346833ce60f0f95d33b4
marcosa <marco,sartorato.dev@gmail.com>
2025-09-15 11:46:53 +0200

    refactor: move out server run and shutdown from the package into the main file

---
+++ internal/hello/message.go
@@ -0,0 +1,44 @@
+package hello
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// MessageRequest represents the incoming JSON payload.
+type MessageRequest struct {
+	Type string `json:"type"`
+	Msg  string `json:"msg,omitempty"`
+}
+
+// MessageResponse for repeat
+type RepeatResponse struct {
+	Type string `json:"type"`
+	Msg  string `json:"msg"`
+}
+
+// MessageResponse for time
+type TimeResponse struct {
+	Type string `json:"type"`
+	Time string `json:"time"`
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	var req MessageRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	switch req.Type {
+	case "repeat":
+		resp := RepeatResponse{Type: "repeat", Msg: req.Msg}
+		json.NewEncoder(w).Encode(resp)
+	case "time":
+		resp := TimeResponse{Type: "time", Time: time.Now().UTC().Format(time.RFC3339)}
+		json.NewEncoder(w).Encode(resp)
+	default:
+		http.Error(w, "unknown type", http.StatusBadRequest)
+	}
+}
