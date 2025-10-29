123603a513d56740502ad787dcf771b2998bb744
marcosa <marco,sartorato.dev@gmail.com>
2025-10-29 21:07:17 +0100

    feat: add timeout conf for HTTP servers

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,14 +4,85 @@ import (
 	"fmt"
 	"math/bits"
 	"strconv"
+	"time"
 )
 
+/*
+Options provides configuration for the HTTP server.
+
+  - Host and Port to bind the HTTP server
+
+  - IdleTimeout is the maximum amount of time to wait for the next request when
+    keep-alives are enabled. If zero, the value of ReadTimeout is used.
+
+  - ReadHeaderTimeout, ReadTimeout, and TimeoutHandler are shown in the diagram below:
+
+    |---------------| <---------|-- connection accepted
+    |	  wait		|			|
+    |---------------|			|
+    | TLS handshake |			| ReadTimeout
+    |---------------| <--|		|
+    |  read header  |	 | ReadHeaderTimeout
+    |---------------| <--|------|------|
+    |   read body   |			|	   |
+    |---------------| <---------|	   | TimeoutHandler
+    |   response    |				   |
+    |---------------| <----------------|
+    |
+    | time
+*/
 type Options struct {
-	Host, Port *string
+	Host, Port                                                  *string
+	ReadHeaderTimeout, ReadTimeout, TimeoutHandler, IdleTimeout time.Duration
 }
 
 type Option func(*Options) error
 
+// WithIdleTimeout returns an Option that sets the IdleTimeout.
+func WithIdleTimeout(timeout int64) Option {
+	return func(o *Options) error {
+		if timeout > 0 {
+			return fmt.Errorf("IdleTimeout must be positive")
+		}
+		o.IdleTimeout = time.Duration(timeout) * time.Millisecond
+		return nil
+	}
+}
+
+// WithTimeoutHandler returns an Option that sets the TimeoutHandler.
+func WithTimeoutHandler(timeout int64) Option {
+	return func(o *Options) error {
+		if timeout > 0 {
+			return fmt.Errorf("TimeoutHandler must be positive")
+		}
+		o.TimeoutHandler = time.Duration(timeout) * time.Millisecond
+		return nil
+	}
+}
+
+// WithReadTimeout returns an Option that sets the ReadTimeout.
+func WithReadTimeout(timeout int64) Option {
+	return func(o *Options) error {
+		if timeout > 0 {
+			return fmt.Errorf("ReadTimeout must be positive")
+		}
+		o.ReadTimeout = time.Duration(timeout) * time.Millisecond
+		return nil
+	}
+}
+
+// WithReadHeaderTimeout returns an Option that sets the ReadHeaderTimeout.
+func WithReadHeaderTimeout(timeout int64) Option {
+	return func(o *Options) error {
+		if timeout > 0 {
+			return fmt.Errorf("ReadHeaderTimeout must be positive")
+		}
+		o.ReadHeaderTimeout = time.Duration(timeout) * time.Millisecond
+		return nil
+	}
+}
+
+// WithHost returns an Option that sets the Host.
 func WithHost(host *string) Option {
 	return func(o *Options) error {
 		if *host == "" {
@@ -22,6 +93,7 @@ func WithHost(host *string) Option {
 	}
 }
 
+// WithPort returns an Option that sets the Port.
 func WithPort(port *string) Option {
 	return func(o *Options) error {
 		const (
--- internal/metricsserver/server.go
+++ internal/metricsserver/server.go
@@ -21,12 +21,22 @@ func Handler() http.Handler {
 func CreateServer(opt cfg.Options) *http.Server {
 	mux := http.NewServeMux()
 
-	mux.Handle("/metrics", Handler())
+	mux.Handle(
+		"/metrics",
+		http.TimeoutHandler(
+			Handler(),
+			opt.TimeoutHandler,
+			"Service Timeout",
+		),
+	)
 
 	addr := net.JoinHostPort(*opt.Host, *opt.Port)
 	server := &http.Server{
-		Addr:    addr,
-		Handler: mux,
+		Addr:              addr,
+		Handler:           mux,
+		ReadTimeout:       opt.ReadTimeout,
+		ReadHeaderTimeout: opt.ReadHeaderTimeout,
+		IdleTimeout:       opt.IdleTimeout,
 	}
 	return server
 }
